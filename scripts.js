2f972c6eff211c4c12b96b1fb9773b7b71fbadbd
adamShaleen <adamshaleen@gmail.com>
2016-12-17 20:27:19 -0600

    added origin* attribute for CORS, parsed out paragraph of text from the JSON object.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,30 @@ $(document).ready(function() {
 
 $.ajax({
     type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=taco",
+    // setting the origin to * seems to get around the cors issue
+    url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&origin=*&page=taco",
     success: function(data) {
-        var result = data.parse.text["*"];
-        console.log(data.parse.text["*"]);
-        $("#searchResult").html(result);
+
+        // JSON returned object
+        var markup = data.parse.text["*"];
+
+        // Setting JSON in a div
+        var display = $("<div></div>").html(markup);
+
+        // removing links
+        display.find("a").each(function() {
+            $(this).replaceWith($(this).html());
+        });
+
+        // removing references
+        display.find("sup").remove();
+
+        // parsing out everything but the paragraph text
+        display = display.find("p");
+
+    // render to HTML
+    $("#searchResult").html(display);
+
     }
 });
 
