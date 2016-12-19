11674b14c40f34b337b7298c7ccbf8f4f1290406
adamShaleen <adamshaleen@gmail.com>
2016-12-18 20:43:16 -0600

    added input value to search query

--- scripts.js
+++ scripts.js
@@ -1,37 +1,61 @@
 $(document).ready(function() {
 
-$.ajax({
-    type: "GET",
-    // setting the origin to * seems to get around the cors issue
-    url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&origin=*&page=taco",
-    success: function(data) {
+$("#wikiSearchButton").click(function() {
 
-        // JSON returned object
-        var markup = data.parse.text["*"];
+    // fade out any content currently on page before loading new content
+    $("#searchResultContainer").hide();
 
-        // Setting JSON in a div
-        var display = $("<div></div>").html(markup);
+    // assign input value to variable
+    var searchTerm = $("#wikiInput").val();
 
-        // removing links
-        display.find("a").each(function() {
-            $(this).replaceWith($(this).html());
-        });
+    $.ajax({
+        type: "GET",
+        // setting the origin to * seems to get around the cors issue
+        url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&origin=*&page=" + searchTerm,
+        success: function(data) {
 
-        // removing references
-        display.find("sup").remove();
+            // JSON returned object
+            var markup = data.parse.text["*"];
 
-        // parsing out everything but the paragraph text
-        display = display.find("p");
+            // Setting JSON in a div
+            var display = $("<div></div>").html(markup);
 
-    // render to HTML
-    $("#searchResult").html(display);
+            // removing links
+            display.find("a").each(function() {
+                $(this).replaceWith($(this).html());
+            });
 
-    }
+            // removing references
+            display.find("sup").remove();
+
+            // title
+            var title = data.parse.displaytitle;
+
+            // paragraph text
+            var paragraph = display.find("p");
+
+            // render to HTML
+            $("#searchTitle").html(title);
+            // $("#image").attr("src", image);
+            $("#searchParagraph").html(paragraph);
+
+            $("#searchResultContainer").fadeIn();
+
+        }
+    });
+
 });
 
+// Random wikipedia entry
+$("#wikiRandomButton").click(function() {
 
-
-
-
+    $.ajax({
+        type: "GET",
+        url: "",    // TODO
+        success: function(data) {
+            console.log(data);
+        }
+    });
+});
 
 });  // end script
