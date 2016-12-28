0145d249b20df7e74e95d5536128f5c8c5601c08
adamShaleen <adamshaleen@gmail.com>
2016-12-27 19:07:04 -0600

    version 1 complete

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
+// hide optional messages upon load
 $("#noEntryMessage").hide();
-
+$("#invalidSearchMessage").hide();
 $("#loader").hide();
 
 $("#wikiSearchButton").click(function() {
@@ -9,18 +10,26 @@ $("#wikiSearchButton").click(function() {
     // fade out any content currently on page before loading new content
     $("#searchResultContainer").hide();
 
+    // start loader animation
     $("#loader").show();
 
     // assign input value to variable
     var searchTerm = $("#wikiInput").val();
 
+    // if the search is valid envoke AJAX HTTP call to wiki API using input value as search
     if (searchTerm) {
         $("#noEntryMessage").hide();
         $.ajax({
             type: "GET",
-            // setting the origin to * seems to get around the cors issue
             url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&origin=*&page=" + searchTerm,
             success: function(data) {
+
+                // if the search isn't a valid word/phrase display the invalid search message
+                if (data.error) {
+                    console.log("HIT");
+                    $("#loader").hide();
+                    $("#invalidSearchMessage").show();
+                }
 
                 // JSON returned object
                 var markup = data.parse.text["*"];
@@ -32,34 +41,28 @@ $("#wikiSearchButton").click(function() {
                 var paragraph = $("<div></div>").html(markup);
 
                 //removing unwanted content from return object
-                paragraph.find("a").each(function() {
-                    $(this).replaceWith($(this).html());
-                });
-
+                paragraph.find("a").each(function() {$(this).replaceWith($(this).html());});
                 paragraph.find("sup").remove();
-
                 paragraph.find("ol").remove();
-
                 paragraph.find("img").remove();
-
                 paragraph.find("table").remove();
-
                 paragraph.find("audio").remove();
-
-                if (paragraph.find("div").hasClass("hatnote")) {
-                    paragraph.find("div").remove();
-                }
+                if (paragraph.find("div").hasClass("hatnote")) {paragraph.find("div").remove();}
 
                 // render to HTML
                 $("#searchTitle").html(title);
                 $("#searchParagraph").html(paragraph);
 
+                // hide and show proper content
                 $("#searchResultContainer").fadeIn();
                 $("#loader").hide();
+                $("#invalidSearchMessage").hide();
             }
         });
      } else {
+         // if search term is not valid hide and show peoper content
         $("#noEntryMessage").show();
+        $("#invalidSearchMessage").hide();
         $("#loader").hide();
     }
 });
